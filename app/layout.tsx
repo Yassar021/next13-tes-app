e9c2809b012ab107fedc3977f5fa9fe698091b3a
Yassar021 <yassar.tkj114@gmail.com>
2023-01-30 01:39:21 +0700

    base next13

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+import Head from './head'
+import Header from './Header'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <Head />
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  )
+}
