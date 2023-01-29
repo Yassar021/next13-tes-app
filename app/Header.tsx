7df8d5ebb06140d5704b642447f18122680553e0
Yassar021 <yassar.tkj114@gmail.com>
2023-01-30 04:09:45 +0700

    update menus navbar

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,6 +5,8 @@ function Header() {
   return (
     <header className='p-5 bg-blue-500'>
       <Link href={'/'} className='px-2 py-1 font-bold text-blue-500 bg-white rounded-lg'>Home</Link>
+      <Link href={'/search'} className='px-2 py-1 mx-4 font-bold text-blue-500 bg-white rounded-lg'>Search</Link>
+      <Link href={'/todos'} className='px-2 py-1 font-bold text-blue-500 bg-white rounded-lg'>Todos</Link>
     </header>
   )
 }
